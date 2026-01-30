a631b6d71d41adf0c5a1e3759aee459c074256ca
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2026-01-30 11:28:30 +0100

    Delete file.py

--- file.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة file.cpython-312.so
-import file
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(file, 'main'):
-    file.main()
-else:
-    print("تم استيراد file بنجاح، ولكن لا توجد دالة main للتشغيل.")
