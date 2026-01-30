93e601f7542c6f705e60049a067fcbf91d0dd0a8
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2026-01-30 11:27:59 +0100

    Delete rico.py

--- rico.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة rico.cpython-312.so
-import rico
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(rico, 'main'):
-    rico.main()
-else:
-    print("تم استيراد rico بنجاح، ولكن لا توجد دالة main للتشغيل.")
