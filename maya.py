c33cbe5b09e7ac056b22a53a756162f064c0956d
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2026-02-15 14:49:51 +0100

    Delete maya.py

--- maya.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة maya.cpython-312.so
-import maya
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(maya, 'main'):
-    maya.main()
-else:
-    print("تم استيراد maya بنجاح، ولكن لا توجد دالة main للتشغيل.")
